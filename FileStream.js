728209bd0f9120c826f888fd0fc1e01ef95d81e7
Yan Qiu <qiuya@microsoft.com>
2021-11-04 19:21:41 +0800

    fix: resolving

--- FileStream.js
+++ FileStream.js
@@ -9,7 +9,7 @@
 //  This is an InputStream that is loaded from a file all at once
 //  when you construct the object.
 //
-import {Lexer} from './Lexer';
+import {InputStream} from './InputStream';
 var isNodeJs = typeof window === 'undefined' && typeof importScripts === 'undefined';
 var fs = isNodeJs ? require("fs") : null;
 
